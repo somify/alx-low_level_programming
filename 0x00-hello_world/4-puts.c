86b47f09d33f8a4ed4925a1d0025e7583cfea3e8
somify <ibekakuchisom@gmail.com>
2022-12-08 20:21:21 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints "programming is like building a multilingual puzzle"
- * Return "0" if properly exited. otherwise "non zero"
+ * Return: 0 if properly exited, otherwise "non zero"
  */
 
 int main(void)
 {
-	puts("\"programing is like building a multiligual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
 
