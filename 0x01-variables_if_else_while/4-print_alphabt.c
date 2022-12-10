f23a4ef6bcaccd969cd4d732867f61fc80a61990
somify <ibekakuchisom@gmail.com>
2022-12-10 15:09:42 -0800

    task 0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char ch;
 
-	for(ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if(ch == 'e' || ch == 'q')
+		if (ch == 'e' || ch == 'q')
 		{
 			continue;
 		}
