66efc6c2a71732c5b25255a1bf5df8f509b9067f
somify <ibekakuchisom@gmail.com>
2022-12-11 02:33:00 -0800

    task 9

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '8'; x++)
+	{
+		for (y = '1'; y <= '9'; y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
