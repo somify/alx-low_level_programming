afce2eb70922d7c35d687e515dec850d35b836b2
somify <ibekakuchisom@gmail.com>
2022-12-13 18:56:50 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * c is the character to be checked
+ * @c: is the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
