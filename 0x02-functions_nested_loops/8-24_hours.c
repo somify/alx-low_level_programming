f923d4e93b00a4bdea6f74821d1788d27935a60f
somify <ibekakuchisom@gmail.com>
2022-12-14 15:29:01 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, 
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
 	int h, m;
-	
 	h = 0;
 	while (h < 24)
 	{
