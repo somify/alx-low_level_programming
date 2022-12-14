79e416db6ec97d0df55556e870f918de9043a055
somify <ibekakuchisom@gmail.com>
2022-12-13 19:49:18 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - gives the absolute value of an integer
+ * @x: is the integer to be checked
+ * Returns: the absolute value of x
+ */
+int _abs(int x)
+{
+	if (x >  0)
+	{
+		return (x);
+	} else if (x == 0)
+	{
+		return ('0');
+	}
+	return (-x)
+}
