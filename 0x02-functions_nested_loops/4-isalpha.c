b60eecccf1f26290faffd8a246fe8d93b92de54b
somify <ibekakuchisom@gmail.com>
2022-12-13 18:39:48 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * c is the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c <= 'a' || c <= 'A') && (c >= 'z' || c >= 'Z'));
+}
