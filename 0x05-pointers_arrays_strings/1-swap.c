5305111a2d0e9de1e303cd37005a2ee03eca3215
somify <ibekakuchisom@gmail.com>
2022-12-20 14:37:12 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swaps the values of two integers
+ * using two input parameter
+ * @a: input parameter 1
+ * @b: input parameter 2
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
