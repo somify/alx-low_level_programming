cb89ef4271a8bbe384eb873fc6aead701cd4ccfa
somify <ibekakuchisom@gmail.com>
2022-12-22 19:01:47 -0800

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	unsigned long int n, x, y, next, x1, x2, y1, y2;
+	unsigned long int n, x, y, next;
 
 	 x = 1;
 	 y = 2;
@@ -20,18 +20,13 @@ int main(void)
 		x = y;
 		y = next;
 	}
-	x1 = x / 1000000000;
-	x2 = x % 1000000000;
-	y1 = y / 1000000000;
-	y2 = y % 1000000000;
 	for (n = 92; n <= 98; n++)
 	{
-		printf("%lu", x1 + (x2 / 1000000000));
-		printf("%lu, ", x2 % 1000000000);
-		x1 = y1;
-		y1 = x1 + y1;
-		x2 = y2;
-		y2 = x2 + y2;
+		printf("%lu", (x / 10000000000000000000));
+		printf("%lu, ", (x % 10000000000000000000));
+		next = x + y;
+		x = y;
+		y = next;
 	}
 	printf("\n");
 	return (0);
