60b78b687f0d45ca5c482f5ba974d7f36d55b1f5
somify <ibekakuchisom@gmail.com>
2022-12-23 19:25:05 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * except 2 and 4
+ *  followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
