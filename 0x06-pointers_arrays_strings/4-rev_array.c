89024b1c557249e92e63d3726442908799faf148
somify <ibekakuchisom@gmail.com>
2023-01-04 19:42:08 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @n: the number of elements of the array
+ * @*a: pointer to int array
+ */
+void reverse_array(int *a, int n)
+{
+	int t, s, e;
+
+	s = 0;
+	e = n-1;
+	while s < e
+	{
+		t = a[s];
+		a[s] = a[e];
+		a[e] = t;
+		s++;
+		e--
+	}
+}
